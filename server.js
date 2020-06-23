335fc3a187b094bff40ad4f62a540310fef1d545
Erfanch <erfan.chinif@gmail.com>
2020-06-23 09:26:59 -0700

    client-side added

--- server.js
+++ server.js
@@ -10,21 +10,10 @@ app.use(bodyParser.urlencoded({extended: true}));
 
 app.use(express.static('./client/dist'));
 
-// app.get('/photogallery', function (req, res) {
-//   console.log(req.body)
-//   res.send(200);
-// })
-
 app.get('/header', function(req, res) {
-  // console.log('request', req.param.body);
-  // console.log('response', res.param);
-  // console.log('gallery', Gallery.find({}).schema )
   Gallery.find({}).exec(function(err,blogs) {
     res.send(blogs);
   })
 });
-
-// app.post('/header', )
-
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`))
